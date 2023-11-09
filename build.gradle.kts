38c168b64ee8fff948cd587530e2e6bd940c2278
Bradley Reynolds <bradley.reynolds@impressdesigns.com>
2023-11-09 04:32:22 +0000

    Test adding more crap from docs
    Signed-off-by: Bradley Reynolds <bradley.reynolds@impressdesigns.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -48,6 +48,9 @@ tasks.withType<Jar> {
 }
 
 sentry {
+    debug.set(true)
+    authToken.set(System.getenv("SENTRY_AUTH_TOKEN"))
+    org.set("impressdesigns")
     projectName.set("charlie")
     includeSourceContext.set(true)
 }
