71071d032988573bf1fff8cdede63255520ac4e2
Bradley Reynolds <bradley.reynolds@impressdesigns.com>
2023-11-08 21:58:40 -0600

    Add Sentry
    Signed-off-by: Bradley Reynolds <bradley.reynolds@impressdesigns.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,46 +1,53 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-	id("org.springframework.boot") version "3.2.0-SNAPSHOT"
-	id("io.spring.dependency-management") version "1.1.3"
-	kotlin("jvm") version "1.9.20"
-	kotlin("plugin.spring") version "1.9.20"
-	id("org.jetbrains.dokka") version "1.9.10"
+    id("org.springframework.boot") version "3.2.0-SNAPSHOT"
+    id("io.spring.dependency-management") version "1.1.3"
+    kotlin("jvm") version "1.9.20"
+    kotlin("plugin.spring") version "1.9.20"
+    id("org.jetbrains.dokka") version "1.9.10"
+    id("io.sentry.jvm.gradle") version "3.14.0"
 }
 
 group = "com.example"
 version = "0.0.1-SNAPSHOT"
 
 java {
-	sourceCompatibility = JavaVersion.VERSION_21
+    sourceCompatibility = JavaVersion.VERSION_21
 }
 
 repositories {
-	mavenCentral()
-	maven { url = uri("https://repo.spring.io/milestone") }
-	maven { url = uri("https://repo.spring.io/snapshot") }
+    mavenCentral()
+    maven { url = uri("https://repo.spring.io/milestone") }
+    maven { url = uri("https://repo.spring.io/snapshot") }
 }
 
 dependencies {
-	implementation(files("vendored/fmjdbc.jar"))
-	implementation("org.springframework.boot:spring-boot-starter-web")
-	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-	implementation("org.jetbrains.kotlin:kotlin-reflect")
-	developmentOnly("org.springframework.boot:spring-boot-devtools")
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
+    implementation(files("vendored/fmjdbc.jar"))
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("io.sentry:sentry-spring-boot-starter-jakarta:6.33.1")
+    developmentOnly("org.springframework.boot:spring-boot-devtools")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
 tasks.withType<KotlinCompile> {
-	kotlinOptions {
-		freeCompilerArgs += "-Xjsr305=strict"
-		jvmTarget = "21"
-	}
+    kotlinOptions {
+        freeCompilerArgs += "-Xjsr305=strict"
+        jvmTarget = "21"
+    }
 }
 
 tasks.withType<Test> {
-	useJUnitPlatform()
+    useJUnitPlatform()
 }
 
 tasks.withType<Jar> {
     archiveFileName.set("${project.name}.jar")
+}
+
+sentry {
+    debug.set(true)
+    includeSourceContext.set(true)
 }
