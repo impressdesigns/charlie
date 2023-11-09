06033e21bd328c2584571a394f25b6967b3cc27a
Bradley Reynolds <bradley.reynolds@impressdesigns.com>
2023-11-09 01:19:39 +0000

    Add vendored dependencies to manifest
    Signed-off-by: Bradley Reynolds <bradley.reynolds@impressdesigns.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,6 +22,7 @@ repositories {
 }
 
 dependencies {
+	implementation(files("vendored/fmjdbc.jar"))
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
