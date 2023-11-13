0d9d8cadd721fd81869ecb382b00dcf3c61fc251
Bradley Reynolds <bradley.reynolds@impressdesigns.com>
2023-11-13 13:43:45 -0600

    Fail startup if the required enviorment variables are not defined (#4)
    Signed-off-by: Bradley Reynolds <bradley.reynolds@impressdesigns.com>

--- src/main/kotlin/com/impressdesigns/charlie/SQL.kt
+++ src/main/kotlin/com/impressdesigns/charlie/SQL.kt
@@ -5,11 +5,9 @@ import java.sql.DriverManager
 
 
 fun connect(): Connection {
-    val url = System.getenv("CHARLIE_JDBC_URI")
-        ?: throw IllegalArgumentException("JDBC URI is required, please set CHARLIE_JDBC_URI")
-    val username = System.getenv("CHARLIE_JDBC_USERNAME")
-        ?: throw IllegalArgumentException("Username is required, please set CHARLIE_JDBC_USERNAME")
-    val password = System.getenv("CHARLIE_JDBC_PASSWORD")
-        ?: throw IllegalArgumentException("Password URI is required, please set CHARLIE_JDBC_PASSWORD")
-    return DriverManager.getConnection(url, username, password)
+    return DriverManager.getConnection(
+        System.getenv("CHARLIE_JDBC_URI"),
+        System.getenv("CHARLIE_JDBC_USERNAME"),
+        System.getenv("CHARLIE_JDBC_PASSWORD"),
+    )
 }
